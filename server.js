ceff263c7a6f9b6d84a414842cc3c696c7f909eb
SirvanD <sirvansh@gmail.com>
2022-02-13 19:20:58 +1100

    added cors

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://bookshelf-client-cqvtcchgd-sirvan.vercel.app/",
+  })
+);
 
 //assign route handling
 app.use("/", indexRouter);
